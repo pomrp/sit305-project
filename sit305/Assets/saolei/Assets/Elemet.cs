b415f241797043e39bdee95578c37a4d2146ada4
martinanswer <1023090715@qq.com>
2018-05-05 22:21:06 +1000

    add minesweeper
    good moving to break the challenge! yoooooooooooooo!

---
+++ sit305/Assets/saolei/Assets/Elemet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elemet : MonoBehaviour {
+
+    public bool mine;
+
+    public Sprite[] emptyTextures;
+    public Sprite mineTextures;
+
+	// Use this for initialization
+	void Start () {
+        mine = Random.value < 0.15;
+
+        int x = (int)transform.position.x;
+        int y = (int)transform.position.y;
+        Grid.elements[x, y] = this;
+		
+	}
+
+    public void loadTexture(int adjacentCount) {
+        if (mine)
+            GetComponent<SpriteRenderer>().sprite = mineTextures;
+        else
+            GetComponent<SpriteRenderer>().sprite = emptyTextures[adjacentCount];
+
+
+    }
+
+    public bool isCovered() {
+        return GetComponent<SpriteRenderer>().sprite.texture.name == "sweep";
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (mine)
+        {
+            Grid.uncoverMines();
+            print("you loose");
+        }
+        else {
+            int x = (int)transform.position.x;
+            int y = (int)transform.position.y;
+            loadTexture(Grid.adjacentMines(x, y));
+            Grid.FFuncover(x, y, new bool[Grid.w, Grid.h]);
+            if (Grid.isFinished())
+                print("you win");
+
+        }
+    }
+
+
+    // Update is called once per frame
+
+}
---
+++ sit305/Assets/saolei/Assets/Gird.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid
+{
+    // The Grid itself
+    public static int w = 5; // this is the width
+    public static int h = 4; // this is the height
+    public static Elemet[,] elements = new Elemet[w, h];
+
+    public static void uncoverMines()
+    {
+        foreach (Elemet elem in elements)
+            if (elem.mine)
+                elem.loadTexture(0);
+    }
+
+    public static bool minAt(int x, int y) {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+            return elements[x, y].mine;
+        return false;
+    }
+
+    public static int adjacentMines(int x, int y) {
+        int count = 0;
+        if (minAt(x, y + 1)) ++count;
+        if (minAt(x+1, y + 1)) ++count;
+        if (minAt(x+1, y)) ++count;
+        if (minAt(x+1, y - 1)) ++count;
+        if (minAt(x, y - 1)) ++count;
+        if (minAt(x-1, y - 1)) ++count;
+        if (minAt(x-1, y)) ++count;
+        if (minAt(x-1, y + 1)) ++count;
+        return count;
+    }
+
+    public static void FFuncover(int x, int y, bool[,] visited)
+    {
+        if (x >= 0 && y >= 0 && x < w && y < h)
+        {
+            if (visited[x, y])
+                return;
+
+            elements[x, y].loadTexture(adjacentMines(x, y));
+
+            if (adjacentMines(x, y) > 0)
+                return;
+
+            visited[x, y] = true;
+
+            FFuncover(x - 1, y, visited);
+            FFuncover(x + 1, y, visited);
+            FFuncover(x, y - 1, visited);
+            FFuncover(x, y + 1, visited);
+
+        }
+    }
+
+    public static bool isFinished() {
+        foreach (Elemet elem in elements)
+            if (elem.isCovered() && !elem.mine)
+                return false;
+        return true;
+
+    }
+}
