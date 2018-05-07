9992497c638478f722664c8168574a00cf2d2371
pomrp <302947314@qq.com>
2018-05-07 21:54:10 +1000

    bricks
    small bricks game

---
+++ Bricks/打砖块/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [Header("Horizontal speed")]
+    public float speedX;
+    Rigidbody2D playerRigidbody2D;
+
+	void Start () {
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        moveLeftOrRight();
+
+    }
+
+    float LeftOrRight()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    void moveLeftOrRight()
+    {
+        playerRigidbody2D.velocity = LeftOrRight() * new Vector2(speedX, 0);
+    }
+}
