07d9bb9ee12341a902da7ab4f893218a8fbf78a1
martinanswer <1023090715@qq.com>
2018-05-09 22:24:54 +1000

    back button
    scence change

---
+++ sit305/Assets/saolei/Assets/changesence.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changesence : MonoBehaviour {
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
