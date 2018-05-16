73568c0c2cecfa58b3a03d3264b00f0412f45b7e
martinanswer <1023090715@qq.com>
2018-05-17 00:47:18 +1000

    perfect game
    Our game was perfect and I like this game so much

---
+++ sit305/Assets/bricks/changescene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changescene : MonoBehaviour {
+
+    public void SceneSwitcher()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
